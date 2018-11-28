d0561f27b34c3d8f09132de8b9e4112c085f4536
Skaydoom <ivan.rabochiy@mail.ru>
2018-11-28 18:53:26 +0300

    Delete

--- prod/ex/exam(Django)/loginsys/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^login/$', views.login, name='loginsys'),
-    url(r'^logout/$', views.logout, name='loginsys'),
-    url(r'^register/$', views.register, name='loginsys'),
-]
--- prod/ex/work_project/orders/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import*
-
-# class SubsForm(forms.ModelForm):
-#
-#     class Meta:
-#         model=Subscriber
-#         exclude=[""]
